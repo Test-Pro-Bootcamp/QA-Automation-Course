977b37a6c5f5a6934b6c5e765ed28722a0220491
Denis Platonov <denis@testpro.io>
2022-10-03 21:04:04 -0700

    Initial commit

---
+++ src/lesson9/Homework9.java
@@ -0,0 +1,11 @@
+package lesson9;
+
+public class Homework9 {
+    public static void main(String[] args) {
+
+//        - Finish up the logic that calculates the balance
+//        - Create a method that withdraws from the balance
+//        - Check that if a bank customer deposits $500, $150 and $35 and then withdraws $40 and $120 the balance is correct
+//        - Print out a meaningful message if it is correct and if not
+    }
+}
