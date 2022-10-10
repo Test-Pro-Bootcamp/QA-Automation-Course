f74d1ba277aa0073bcd88d2a8ff46a91ac5b091c
Azat Zakuanov <azat@testpro.io>
2022-10-10 15:05:30 -0700

    added sets and maps lessons

---
+++ src/lesson11/SetsLesson.java
@@ -0,0 +1,9 @@
+package lesson11;
+
+public class SetsLesson {
+
+    public static void main(String[] args) {
+        System.out.println("Sets lesson");
+    }
+
+}
---
+++ src/lesson12/MapsLesson.java
@@ -0,0 +1,9 @@
+package lesson12;
+
+public class MapsLesson {
+
+    public static void main(String[] args) {
+        System.out.println("Maps lesson");
+    }
+
+}
